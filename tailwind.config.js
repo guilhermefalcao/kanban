bb2f83b8b192f44561f23671af92c1b057ef990b
Guilherme Falcao <guilhermelfalcao@gmail.com>
2022-11-19 21:41:44 -0800

    customize color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      purple: "#635fc7",
+      lightPurple: "#a8a4ff",
+      gray900: "#000112",
+      gray800: "#20212c",
+      gray700: "#2b2c37",
+      gray600: "#3e3f4e",
+      gray400: "#828fa3",
+      gray300: "#e4ebfa",
+      gray: "#f4f7fd",
+      white: "#ffffff",
+      red500: "#ea5555",
+      red100: "#ff9898",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
